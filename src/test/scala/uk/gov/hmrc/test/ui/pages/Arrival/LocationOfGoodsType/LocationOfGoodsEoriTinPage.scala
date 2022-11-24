5e3c4ab3a5ed59480d97046b72923bceb921b9f5
Gosifo <19684916+Gosifo@users.noreply.github.com>
2022-11-24 12:07:18 +0000

    updated page title Consignee Eori Tin Page and Location Of Goods Eori Tin Page

--- src/test/scala/uk/gov/hmrc/test/ui/pages/Arrival/LocationOfGoodsType/LocationOfGoodsEoriTinPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/Arrival/LocationOfGoodsType/LocationOfGoodsEoriTinPage.scala
@@ -20,5 +20,5 @@ import uk.gov.hmrc.test.ui.pages.StringPage
 
 object LocationOfGoodsEoriTinPage extends StringPage {
 
-  override def title(args: String*): String = "What is the EORI number or TIN for the location of goods?"
+  override def title(args: String*): String = "What is the EORI number or Trader Identification Number (TIN) for the location of goods?"
 }
