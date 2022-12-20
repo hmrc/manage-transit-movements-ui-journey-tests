2d09f45485fef1a9cf774b7411149d442bd75584
John Whitfield <john.whitfield@digital.hmrc.gov.uk>
2022-12-20 10:48:41 +0000

    New stepdefs & pages added for scenario 15 & 16 in Manage feature

---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/Manage/ControlDecisionPage.scala
@@ -0,0 +1,9 @@
+package uk.gov.hmrc.test.ui.pages.Manage
+
+import uk.gov.hmrc.test.ui.pages.Page
+
+object NoReleaseForTransitPage extends Page {
+
+   override def title(args: String*): String = "There is a problem with this departure declaration"
+
+}
