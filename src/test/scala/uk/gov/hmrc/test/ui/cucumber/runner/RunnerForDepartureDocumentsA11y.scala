fbc94c890a7a2d536eff33a358b42b0cb2e52c7f
John Whitfield <john.whitfield@digital.hmrc.gov.uk>
2023-03-16 16:34:19 +0000

    Bracket removed from runner file

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/runner/RunnerForDepartureDocumentsA11y.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/runner/RunnerForDepartureDocumentsA11y.scala
@@ -24,6 +24,6 @@ import org.junit.runner.RunWith
   features = Array("src/test/resources/features"),
   glue = Array("uk.gov.hmrc.test.ui.cucumber.stepdefs"),
   plugin = Array("pretty", "html:target/cucumber", "json:target/cucumber.json"),
-  tags = "(@departureDocumentsA11y"
+  tags = "@departureDocumentsA11y"
 )
 class RunnerForDepartureDocumentsA11y {}
