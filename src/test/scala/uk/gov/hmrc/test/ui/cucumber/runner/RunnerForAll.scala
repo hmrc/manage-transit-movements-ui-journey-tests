4e807f4b23f3aa63171846f509ddfee47fed9909
georgina.duckworth <georgina.duckworth@digital.hmrc.gov.uk>
2024-03-26 11:21:31 +0000

    Update runners
    Add new tag to runners

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/runner/RunnerForAll.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/runner/RunnerForAll.scala
@@ -27,6 +27,6 @@ import org.junit.runner.RunWith
   tags =
     "@departureGBe2e or @departureXIe2e or @departureGuaranteeDetails or @departureHub or @departureItemDetails or " +
       "@departureRouteDetails or @departureTraderDetails or @departureTransportDetails or @departureDocuments or " +
-      "@arrivalE2e or @cancellation or @managehub or @manageDraftDeparture or @unloading_remarks or @departureAmendment or @presentationNotificationIe15,@presentationNotificationIe13"
+      "@arrivalE2e or @cancellation or @managehub or @manageDraftDeparture or @unloading_remarks or @departureAmendment or @presentationNotificationIe15,@presentationNotificationIe13 or @UnloadingCrossCheck"
 )
 class RunnerForAll {}
