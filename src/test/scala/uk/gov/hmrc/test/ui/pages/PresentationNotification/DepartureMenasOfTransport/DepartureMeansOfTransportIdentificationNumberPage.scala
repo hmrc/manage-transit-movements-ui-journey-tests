fb1ca2aff7eac5499a9c6a8ad00d651b6ba4f68b
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2024-04-24 14:55:07 +0100

    Presentation notification changes

--- src/test/scala/uk/gov/hmrc/test/ui/pages/PresentationNotification/DepartureMenasOfTransport/DepartureMeansOfTransportIdentificationNumberPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/PresentationNotification/DepartureMenasOfTransport/DepartureMeansOfTransportIdentificationNumberPage.scala
@@ -21,6 +21,6 @@ import uk.gov.hmrc.test.ui.pages.StringPage
 object DepartureMeansOfTransportIdentificationNumberPage extends StringPage {
 
   override def title(args: String*): String =
-    String.format("What is the identification number for the departure means of transport?")
+    String.format("What is the identification for the departure means of transport?")
 
 }
