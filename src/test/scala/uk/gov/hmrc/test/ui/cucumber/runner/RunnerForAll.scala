7769e1156c2299d884e4f1bb9d03b3a5f97f6f72
rajukakumanu86 <raju.kakumanu@digital.hmrc.gov.uk>
2024-05-30 15:18:36 +0100

    Updated arrival test

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/runner/RunnerForAll.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/runner/RunnerForAll.scala
@@ -25,6 +25,8 @@ import org.junit.runner.RunWith
   glue = Array("uk.gov.hmrc.test.ui.cucumber.stepdefs"),
   plugin = Array("pretty", "html:target/cucumber", "json:target/cucumber.json"),
   tags =
-    "@departureGBe2e"
+    "@departureGBe2e or @departureXIe2e or @departureGuaranteeDetails or @departureHub or @departureItemDetails or " +
+      "@departureRouteDetails or @departureTraderDetails or @departureTransportDetails or @departureDocuments or " +
+      "@arrivalE2e or @cancellation or @managehub or @manageDraftDeparture or @unloading_remarks or @departureAmendment or @presentationNotificationIe15,@presentationNotificationIe13 or @UnloadingCrossCheck"
 )
 class RunnerForAll {}
