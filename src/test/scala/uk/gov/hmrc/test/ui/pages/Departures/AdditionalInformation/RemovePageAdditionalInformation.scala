f45117355b9dd0531020db2851ebf6686d77e9ca
georgina.duckworth <georgina.duckworth@digital.hmrc.gov.uk>
2024-06-13 15:09:55 +0100

    CTCP-5156
    Update to page

--- src/test/scala/uk/gov/hmrc/test/ui/pages/Departures/AdditionalInformation/RemovePageAdditionalInformation.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/Departures/AdditionalInformation/RemovePageAdditionalInformation.scala
@@ -20,6 +20,7 @@ import uk.gov.hmrc.test.ui.pages.YesNoPage
 
 object RemovePageAdditionalInformation extends YesNoPage {
 
-  override def title(args: String*): String = "Are you sure you want to remove this additional information from all items?"
+  override def title(args: String*): String =
+    "Are you sure you want to remove this additional information from all items?"
 
 }
