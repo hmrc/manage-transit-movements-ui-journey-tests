c945ff8c885fc372d38b632539479d3facd1f040
neda.vazharova <14248739+vajarova@users.noreply.github.com>
2024-06-17 14:54:02 +0100

    CTCP-3880: Update item level supply chain actor title

--- src/test/scala/uk/gov/hmrc/test/ui/pages/Departures/Items/SupplyChainActorEoriTinItemPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/Departures/Items/SupplyChainActorEoriTinItemPage.scala
@@ -21,6 +21,6 @@ import uk.gov.hmrc.test.ui.pages.StringPage
 object SupplyChainActorEoriTinItemPage extends StringPage {
 
   override def title(args: String*): String =
-    String.format("What is the EORI number or Trader Identification Number (TIN) for the %s?", args: _*)
+    String.format("What is the EORI number or Third Country Unique Identification Number (TCUIN) for the %s?", args: _*)
 
 }
