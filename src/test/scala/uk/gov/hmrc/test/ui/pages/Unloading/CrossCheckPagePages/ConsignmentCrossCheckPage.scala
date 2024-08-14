d63db7f7a402c32331a54040c3415a1b96a9fa85
JSquires91 <57271478+JSquires91@users.noreply.github.com>
2024-08-14 11:36:38 +0100

    Adding a few extra bits

--- src/test/scala/uk/gov/hmrc/test/ui/pages/Unloading/CrossCheckPagePages/ConsignmentCrossCheckPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/Unloading/CrossCheckPagePages/ConsignmentCrossCheckPage.scala
@@ -21,7 +21,7 @@ import uk.gov.hmrc.test.ui.pages.Page
 object ConsignmentCrossCheckPage extends Page {
 
   override def title(args: String*): String =
-    "Enter the discrepancies between the transit movement and unloading permission"
+    "Enter the discrepancies between the transit movement and unloading permission - Arrival notifications"
 
   def clickLinkById(text: String): Unit = {
     val id = s"change-${text.replace(" ", "-").toLowerCase}"
