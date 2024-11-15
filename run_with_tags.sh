3cadac765bbd95563a1229423c60384e254dec1f
JSquires91 <57271478+JSquires91@users.noreply.github.com>
2024-11-15 14:12:44 +0000

    revert runner script back to Chrome :(

--- run_with_tags.sh
+++ run_with_tags.sh
@@ -5,4 +5,4 @@ echo Running $1 test suite
 # Scalafmt checks have been separated from the test command to avoid OutOfMemoryError in Jenkins
 sbt scalafmtCheckAll scalafmtSbtCheck
 
-sbt -Denvironment=local -Dbrowser=firefox "testOnly uk.gov.hmrc.test.ui.cucumber.runner.RunnerFor$1"
+sbt -Denvironment=local -Dbrowser=chrome "testOnly uk.gov.hmrc.test.ui.cucumber.runner.RunnerFor$1"
