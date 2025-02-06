83458415412a2842dbf856b6aff57e4cb4ed8849
JSquires91 <57271478+JSquires91@users.noreply.github.com>
2025-02-06 14:34:52 +0000

    Updated Json and Cache helpers for departure declarations

--- src/test/scala/uk/gov/hmrc/test/ui/utils/CacheHelper.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/utils/CacheHelper.scala
@@ -19,21 +19,18 @@ package uk.gov.hmrc.test.ui.utils
 import org.mongodb.scala.bson.BsonDocument
 import org.mongodb.scala.{SingleObservableFuture, bsonDocumentToDocument}
 import play.api.libs.json.Json
-import uk.gov.hmrc.test.ui.conf.TestConfiguration
-import uk.gov.hmrc.test.ui.cucumber.stepdefs.World
 
 object CacheHelper extends HttpClient with FileHelper with JsonHelper with MongoHelper {
 
-  private def headers: Seq[(String, String)] = Seq(
-    ("Authorization", World.bearerToken)
-  )
-
-  private val proxy = TestConfiguration.url("manage-transit-movements-departure-frontend")
-
   def submitDepartureAnswers(fileName: String, lrn: String, eoriNumber: String): Unit = {
-    val url  = s"$proxy/test-only/user-answers/${World.sessionId}"
-    val json = getJson(fileName).withLrn(lrn).withEoriNumber(eoriNumber)
-    post(url, json, headers)
+    val json = getJson(fileName).withLrn(lrn).withEoriNumber(eoriNumber).withCreatedAt().withLastUpdated().withId()
+    awaitResult {
+      mongoClient
+        .getDatabase("manage-transit-movements-departure-cache")
+        .getCollection("user-answers")
+        .insertOne(bsonDocumentToDocument(BsonDocument(Json.stringify(json))))
+        .toFuture()
+    }
   }
 
   def submitArrivalAnswers(fileName: String, mrn: String, eoriNumber: String): Unit = {
