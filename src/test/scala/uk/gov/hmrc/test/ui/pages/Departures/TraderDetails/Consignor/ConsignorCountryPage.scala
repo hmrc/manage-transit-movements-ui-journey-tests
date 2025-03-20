9575dc24220112c01f65588796bbeee5c57f035e
JSquires91 <57271478+JSquires91@users.noreply.github.com>
2025-03-20 13:46:36 +0000

    CTCP-6361: Updated trader details country pages

--- src/test/scala/uk/gov/hmrc/test/ui/pages/Departures/TraderDetails/Consignor/ConsignorCountryPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/Departures/TraderDetails/Consignor/ConsignorCountryPage.scala
@@ -22,4 +22,6 @@ object ConsignorCountryPage extends SelectPage {
 
   override def title(args: String*): String = "Which country is the consignor based in?"
 
+  override val id: String = "country"
+
 }
