447203b7c59be1e537f37ba944cfc2ce41d7c697
vajarova <14248739+vajarova@users.noreply.github.com>
2026-02-18 13:00:51 +0000

    CTCP6-207: Supply chain actor step def fix

--- src/test/scala/specsteps/SupplyChainActorStepDefSteps.scala
+++ src/test/scala/specsteps/SupplyChainActorStepDefSteps.scala
@@ -4,7 +4,7 @@ import uk.gov.hmrc.test.ui.pages.Departures.Transport.SupplyChainActor.*
 object SupplyChainActorStepDefSteps {
 
   // ^(?:I )?choose radio option (.*) on the 'Do you want to add a supply chain actor for all items\?' page$
-  def and(?I)?chooseRadioOptionXOnThe'DoYouWantToAddASupplyChainActorForAllItems\?'Page(answer: String): Unit = {
+  def andIchooseRadioOptionXOnTheDoYouWantToAddASupplyChainActorForAllItemsPage(answer: String): Unit = {
     AddSupplyChainActorPage
             .loadPage()
             .select(answer)
@@ -12,7 +12,7 @@ object SupplyChainActorStepDefSteps {
   }
 
   // ^(?:I )?choose radio option (.*) on the 'Which type of supply chain actor do you want to add\?' page$
-  def and(?I)?chooseRadioOptionXOnThe'WhichTypeOfSupplyChainActorDoYouWantToAdd\?'Page(answer: String): Unit = {
+  def andIchooseRadioOptionXOnTheWhichTypeOfSupplyChainActorDoYouWantToAddPage(answer: String): Unit = {
     TypeSupplyChainActorPage
             .loadPage()
             .select(answer)
@@ -20,7 +20,7 @@ object SupplyChainActorStepDefSteps {
   }
 
   // ^(?:I )?enter (.+) on the 'What is the EORI number or Third Country Unique Identification Number TCUIN for the (.*)\?' page$
-  def and(?I)?enter(.+)OnThe'WhatIsTheEORINumberOrThirdCountryUniqueIdentificationNumberTCUINForTheX\?'Page(answer: String, typeVal: String): Unit = {
+  def andIenterOnTheWhatIsTheEORINumberOrThirdCountryUniqueIdentificationNumberTCUINForTheXPage(answer: String, typeVal: String): Unit = {
     EoriTinSupplyChainActorPage
           .loadPage(typeVal)
           .fillInput(answer)
@@ -28,7 +28,7 @@ object SupplyChainActorStepDefSteps {
   }
 
   // ^(?:I )?choose radio option (.*) on the 'You have added (.+) supply chain actors?' page$
-  def and(?I)?chooseRadioOptionXOnThe'YouHaveAdded(.+)SupplyChainActors?'Page(answer: String, numberOfActors: String): Unit = {
+  def andIchooseRadioOptionXOnTheYouHaveAddedSupplyChainActorsPage(answer: String, numberOfActors: String): Unit = {
     AddAnotherSupplyChainActorPage
             .loadPage(numberOfActors)
             .select(answer)
@@ -36,14 +36,14 @@ object SupplyChainActorStepDefSteps {
   }
 
   // ^(?:I )?choose to click on (.*) link on the 'You have added (.+) supply chain actors' page$
-  def when(?I)?chooseToClickOnXLinkOnThe'YouHaveAdded(.+)SupplyChainActors'Page(sectionLink: String, numberOfActors: String): Unit = {
+  def whenIchooseToClickOnXLinkOnTheYouHaveAddedSupplyChainActorsPage(sectionLink: String, numberOfActors: String): Unit = {
     AddAnotherSupplyChainActorPage
             .loadPage(numberOfActors)
             .clickByPartialLinkText(sectionLink)
   }
 
   // ^(?:I )?choose radio option (.*) on the Are you sure you want to remove this supply chain actor page$
-  def and(?I)?chooseRadioOptionXOnTheAreYouSureYouWantToRemoveThisSupplyChainActorPage(answer: String): Unit = {
+  def andIchooseRadioOptionXOnTheAreYouSureYouWantToRemoveThisSupplyChainActorPage(answer: String): Unit = {
     RemoveSupplyChainActorPage
             .loadPage()
             .select(answer)
