d6c2e67912bf856ba28bb0054e7d156552ffd6dc
vajarova <14248739+vajarova@users.noreply.github.com>
2026-02-18 13:02:25 +0000

    CTCP6-207: Task list step def fix

--- src/test/scala/specsteps/TaskListStepDefSteps.scala
+++ src/test/scala/specsteps/TaskListStepDefSteps.scala
@@ -4,33 +4,33 @@ import uk.gov.hmrc.test.ui.pages.Departures.DeclarationSummary.DeclarationSummar
 object TaskListStepDefSteps {
 
   // ^(?:I )?(?:should )?be on the 'Declaration summary' page$
-  def then(?I)?(?should)?beOnThe'DeclarationSummary'Page(): Unit = {
+  def thenIshouldbeOnTheDeclarationSummaryPage(): Unit = {
     DeclarationSummaryPage
           .loadPage()
   }
 
   // ^(?:I )?click the (.+) link on the 'Declaration summary' page$
-  def then(?I)?clickThe(.+)LinkOnThe'DeclarationSummary'Page(answer: String): Unit = {
+  def thenIclickTheLinkOnTheDeclarationSummaryPage(answer: String): Unit = {
     DeclarationSummaryPage
           .loadPage()
           .selectDeclarationSection(answer)
   }
 
   // ^(?:I )?click on the (.+) link on the 'Declaration summary' page$
-  def then(?I)?clickOnThe(.+)LinkOnThe'DeclarationSummary'Page(answer: String): Unit = {
+  def thenIclickOnTheLinkOnTheDeclarationSummaryPage(answer: String): Unit = {
     DeclarationSummaryPage
           .loadPage()
           .selectDeclarationSection(answer)
   }
 
   // ^(?:I )?should see (.*) status for (.+) on the 'Declaration summary' page$
-  def and(?I)?shouldSeeXStatusFor(.+)OnThe'DeclarationSummary'Page(status: String, section: String): Unit = {
+  def andIshouldSeeXStatusForOnTheDeclarationSummaryPage(status: String, section: String): Unit = {
     DeclarationSummaryPage
             .checkStatus(section, status)
   }
 
   // ^(?:I )?click the (?:Confirm and resend|Confirm and send) button on the 'Declaration summary' page$
-  def and(?I)?clickThe(?ConfirmAndResend|ConfirmAndSend)ButtonOnThe'DeclarationSummary'Page(): Unit = {
+  def andIclickTheConfirmAndResendConfirmAndSendButtonOnTheDeclarationSummaryPage(): Unit = {
     DeclarationSummaryPage
           .loadPage()
           .submitPage()
