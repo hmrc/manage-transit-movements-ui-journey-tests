ee4b38f73cf4b8f3b398f3f313e01cd14e58877e
JSquires91 <57271478+JSquires91@users.noreply.github.com>
2026-02-25 13:50:34 +0000

    CTCP6-207: Formatting MTM Arrivals P6 Spec

--- src/test/scala/convertedSpecs/ManageTransitMovementsArrivalsP6Spec.scala
+++ src/test/scala/convertedSpecs/ManageTransitMovementsArrivalsP6Spec.scala
@@ -48,31 +48,21 @@ class ManageTransitMovementsArrivalsP6Spec extends BaseSpec with GivenWhenThen {
       andILoginWithIDX("1234567890")
 
       When("I submit an Phase6 IE007 Arrival Notification")
-      // Possible match (best=1.00)
       givenIsubmitAFile("Phase6 IE007 Arrival Notification")
 
       And("I submit an IE043 Unloading Permission With Seals")
-      // Possible match (best=1.00)
       givenIsubmitAFile("IE043 Unloading Permission With Seals")
 
       When("I click on the View arrival notifications link on the Manage your transit movements page")
-      // Possible match (best=0.95)
       givenIclickOnTheLinkOnTheManageYourTransitMovementsPage("View arrival notifications")
-      // --- Other possible matches ---
-      // givenIClickOnTheLinkOnTheArrivalNotificationsPage() [0.79] (ArrivalNotificationsStepDefSteps.scala) pattern: (?:I )?click on the (.+) link on the 'Arrival notifications' page
 
       And("I click on the Make unloading remarks link for MRN 25GB000246TK0E6WJ1 on the Arrival notifications page")
-      // Possible match (best=0.90)
       andIClickOnTheXLinkForMRNXOnTheArrivalNotificationsPage("Make unloading remarks", "25GB000246TK0E6WJ1")
-      // --- Other possible matches ---
-      // givenIClickOnTheLinkOnTheArrivalNotificationsPage() [0.79] (ArrivalNotificationsStepDefSteps.scala) pattern: (?:I )?click on the (.+) link on the 'Arrival notifications' page
 
       Then("I should be on the Do you want to use the revised unloading procedure? page")
-      // ⚠️ No step-def match found for: I should be on the Do you want to use the revised unloading procedure? page
       thenIshouldBeOnTheDoYouWantToUseTheRevisedUnloadingProcedurePage()
 
       And("I sign out")
-      // Possible match (best=1.00)
       andISignOut()
 
     }
@@ -82,43 +72,30 @@ class ManageTransitMovementsArrivalsP6Spec extends BaseSpec with GivenWhenThen {
       andILoginWithIDX("1234567890")
 
       When("I submit an Phase6 IE007 Arrival Notification")
-      // Possible match (best=1.00)
       givenIsubmitAFile("Phase6 IE007 Arrival Notification")
 
       And("I submit an IE043 Unloading Permission With Seals")
-      // Possible match (best=1.00)
       givenIsubmitAFile("IE043 Unloading Permission With Seals")
 
       And("I submit an IE044 Unloading Remarks Notification With Seals")
-      // Possible match (best=1.00)
       givenIsubmitAFile("IE044 Unloading Remarks Notification With Seals")
 
       And("I submit an IE057 Unloading Remarks Rejection")
-      // Possible match (best=1.00)
       givenIsubmitAFile("IE057 Unloading Remarks Rejection")
 
       When("I click on the View arrival notifications link on the Manage your transit movements page")
-      // Possible match (best=0.95)
       givenIclickOnTheLinkOnTheManageYourTransitMovementsPage("View arrival notifications")
-      // --- Other possible matches ---
-      // givenIClickOnTheLinkOnTheArrivalNotificationsPage() [0.79] (ArrivalNotificationsStepDefSteps.scala) pattern: (?:I )?click on the (.+) link on the 'Arrival notifications' page
 
       Then("I should be on the Arrival notifications page")
-      // Possible match (best=1.00)
       thenIShouldBeOnTheArrivalNotificationsPage()
 
       And("I click on the View errors link for MRN 25GB000246TK0E6WJ1 on the Arrival notifications page")
-      // Possible match (best=0.90)
       andIClickOnTheXLinkForMRNXOnTheArrivalNotificationsPage("View errors", "25GB000246TK0E6WJ1")
-      // --- Other possible matches ---
-      // givenIClickOnTheLinkOnTheArrivalNotificationsPage() [0.79] (ArrivalNotificationsStepDefSteps.scala) pattern: (?:I )?click on the (.+) link on the 'Arrival notifications' page
 
       Then("I should be on the Review unloading remarks errors page")
-      // Possible match (best=1.00)
       thenIshouldBeOnTheReviewUnloadingRemarksErrorsPage()
 
       And("I sign out")
-      // Possible match (best=1.00)
       andISignOut()
 
     }
@@ -128,35 +105,24 @@ class ManageTransitMovementsArrivalsP6Spec extends BaseSpec with GivenWhenThen {
       andILoginWithIDX("1234567890")
 
       When("I submit an Phase6 IE007 Arrival Notification")
-      // Possible match (best=1.00)
       givenIsubmitAFile("Phase6 IE007 Arrival Notification")
 
       And("I submit an IE057 Arrival Notification Rejection")
-      // Possible match (best=1.00)
       givenIsubmitAFile("IE057 Arrival Notification Rejection")
 
       When("I click on the View arrival notifications link on the Manage your transit movements page")
-      // Possible match (best=0.95)
       givenIclickOnTheLinkOnTheManageYourTransitMovementsPage("View arrival notifications")
-      // --- Other possible matches ---
-      // givenIClickOnTheLinkOnTheArrivalNotificationsPage() [0.79] (ArrivalNotificationsStepDefSteps.scala) pattern: (?:I )?click on the (.+) link on the 'Arrival notifications' page
 
       Then("I should be on the Arrival notifications page")
-      // Possible match (best=1.00)
       thenIShouldBeOnTheArrivalNotificationsPage()
 
       And("I click on the View errors link for MRN 25GB000246TK0E6WJ1 on the Arrival notifications page")
-      // Possible match (best=0.90)
       andIClickOnTheXLinkForMRNXOnTheArrivalNotificationsPage("View errors", "25GB000246TK0E6WJ1")
-      // --- Other possible matches ---
-      // givenIClickOnTheLinkOnTheArrivalNotificationsPage() [0.79] (ArrivalNotificationsStepDefSteps.scala) pattern: (?:I )?click on the (.+) link on the 'Arrival notifications' page
 
       Then("I should be on the Review notification errors page")
-      // Possible match (best=1.00)
       thenIShouldBeOnTheReviewNotificationErrorsPage()
 
       And("I sign out")
-      // Possible match (best=1.00)
       andISignOut()
 
     }
