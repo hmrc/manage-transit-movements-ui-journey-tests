26c0a0eccf004e5954eb0d2e33f89c97bf1916e7
vajarova <14248739+vajarova@users.noreply.github.com>
2026-03-03 14:58:33 +0000

    CTCP6-207: Update to wip shell script to be able to pass the spec file as an argument to run

--- src/test/scala/convertedSpecs/DepartureFEs/TransportDetailsSpec.scala
+++ src/test/scala/convertedSpecs/DepartureFEs/TransportDetailsSpec.scala
@@ -135,8 +135,12 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I choose radio option Maritime on the 'Which inland mode of transport are you using?' page")
       andIchooseRadioOptionXOnTheWhichInlandModeOfTransportAreYouUsingPage("Maritime")
 
-      And("I choose radio option Name of a sea-going vessel on the inland mode 'Which identification do you want to use for this vehicle?' page")
-      andIchooseRadioOptionXOnTheInlandModeWhichIdentificationDoYouWantToUseForThisVehiclePage("Name of a sea-going vessel")
+      And(
+        "I choose radio option Name of a sea-going vessel on the inland mode 'Which identification do you want to use for this vehicle?' page"
+      )
+      andIchooseRadioOptionXOnTheInlandModeWhichIdentificationDoYouWantToUseForThisVehiclePage(
+        "Name of a sea-going vessel"
+      )
 
       And("I enter registration number GB1234567 on the What is the identification number for this? page")
       andIenterRegistrationNumberOnTheWhatIsTheIdentificationNumberForThisPage("GB1234567")
@@ -150,8 +154,12 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I select radio option Yes on the You have added 1 departure means of transport page")
       andIselectRadioOptionXOnTheYouHaveAddedDepartureMeansOfTransportPage("Yes", "1")
 
-      And("I choose radio option Name of a sea-going vessel on the inland mode 'Which identification do you want to use for this vehicle?' page")
-      andIchooseRadioOptionXOnTheInlandModeWhichIdentificationDoYouWantToUseForThisVehiclePage("Name of a sea-going vessel")
+      And(
+        "I choose radio option Name of a sea-going vessel on the inland mode 'Which identification do you want to use for this vehicle?' page"
+      )
+      andIchooseRadioOptionXOnTheInlandModeWhichIdentificationDoYouWantToUseForThisVehiclePage(
+        "Name of a sea-going vessel"
+      )
 
       And("I enter registration number GB1234567 on the What is the identification number for this? page")
       andIenterRegistrationNumberOnTheWhatIsTheIdentificationNumberForThisPage("GB1234567")
@@ -181,15 +189,18 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I choose radio option Yes on the border means Do you want to add identification for this vehicle? page")
       andIchooseRadioOptionXOnTheBorderMeansDoYouWantToAddIdentificationForThisVehiclePage("Yes")
 
-      And("I choose radio option Name of a sea-going vessel on the border mode 'Which identification do you want to use for this vehicle?' page")
-      andIchooseRadioOptionXOnTheBorderModeWhichIdentificationDoYouWantToUseForThisVehiclePage("Name of a sea-going vessel")
+      And(
+        "I choose radio option Name of a sea-going vessel on the border mode 'Which identification do you want to use for this vehicle?' page"
+      )
+      andIchooseRadioOptionXOnTheBorderModeWhichIdentificationDoYouWantToUseForThisVehiclePage(
+        "Name of a sea-going vessel"
+      )
 
       And("I enter identification GB1234567 on the What is the identification number for this? page")
       andIenterIdentificationOnTheWhatIsTheIdentificationNumberForThisPage("GB1234567")
 
       And("I select United Kingdom on the border mode What country is this vehicle registered to? page")
       andIselectOnTheBorderModeWhatCountryIsThisVehicleRegisteredToPage("United Kingdom")
-
 
       // ## Border means office of transit
       And("I select Basel (DE004058) on the 'Where is the customs office at the border?' page")
@@ -208,8 +219,12 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I choose radio option Yes on the You have added 1 border means of transport page")
       whenIchooseRadioOptionXOnTheYouHaveAddedXBorderMeansOfTransportPage("Yes", "1")
 
-      And("I choose radio option Registration number of the road vehicle on the border mode 'Which identification do you want to use for this vehicle?' page")
-      andIchooseRadioOptionXOnTheBorderModeWhichIdentificationDoYouWantToUseForThisVehiclePage("Registration number of the road vehicle")
+      And(
+        "I choose radio option Registration number of the road vehicle on the border mode 'Which identification do you want to use for this vehicle?' page"
+      )
+      andIchooseRadioOptionXOnTheBorderModeWhichIdentificationDoYouWantToUseForThisVehiclePage(
+        "Registration number of the road vehicle"
+      )
 
       And("I enter identification number GB15REG on What is the identification number for this? page")
       andIenterIdentificationOnTheWhatIsTheIdentificationNumberForThisPage("GB15REG")
@@ -306,7 +321,9 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I choose radio option Yes on the Do you want to add a carrier? page")
       andIChooseRadioOptionXOnTheDoYouWantToAddACarrierPage("Yes")
 
-      And("I enter GB123456123456 on the 'What is the carrier’s EORI number or Third Country Unique Identification Number TCUIN?' page")
+      And(
+        "I enter GB123456123456 on the 'What is the carrier’s EORI number or Third Country Unique Identification Number TCUIN?' page"
+      )
       andIenterOnTheWhatIsTheCarriersEORINumberOrThirdCountryUniqueIdentificationNumberTCUINPage("GB123456123456")
 
       And("I choose radio option Yes on the 'Do you want to add a contact for the carrier?' page")
@@ -349,7 +366,7 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I choose radio option Yes on the You have added 1 transport equipment page")
       andIchooseRadioOptionXOnTheYouHaveAddedXTransportEquipmentPage("Yes", "1")
 
-      //Transport equipment 2
+      // Transport equipment 2
 
       And("I choose radio option Yes on the 'Do you want to add a container identification number?' page")
       andIchooseRadioOptionXOnTheDoYouWantToAddAContainerIdentificationNumberPage("Yes")
@@ -401,7 +418,9 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I click the Remove link on the 'You have added 2 additional references for all items?' page")
       andIclickTheXLinkOnTheYouHaveAddedXAdditionalReferencesForAllItemsPage("Remove", "2")
 
-      And("I click radio option Yes on the 'Are you sure you want to remove this additional reference from all items?' page")
+      And(
+        "I click radio option Yes on the 'Are you sure you want to remove this additional reference from all items?' page"
+      )
       andIclickRadioOptionXOnTheAreYouSureYouWantToRemoveThisAdditionalReferenceFromAllItemsPage("Yes")
 
       And("I click radio option Yes on the 'You have added 1 additional reference for all items' page")
@@ -412,8 +431,12 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I click radio option Yes on the Do you want to add any additional information for all items? page")
       andIclickRadioOptionXOnTheDoYouWantToAddAnyAdditionalInformationForAllItemsPage("Yes")
 
-      And("I select 20100 - Export from one EFTA country subject to restriction or export from the Union subject to restriction on the 'What type of additional information do you want to add?' page")
-      andIselectOnTheWhatTypeOfAdditionalInformationDoYouWantToAddPage("20100 - Export from one EFTA country subject to restriction or export from the Union subject to restriction")
+      And(
+        "I select 20100 - Export from one EFTA country subject to restriction or export from the Union subject to restriction on the 'What type of additional information do you want to add?' page"
+      )
+      andIselectOnTheWhatTypeOfAdditionalInformationDoYouWantToAddPage(
+        "20100 - Export from one EFTA country subject to restriction or export from the Union subject to restriction"
+      )
 
       And("I click radio option Yes on the 'Do you want to add any comments for the additional information?' page")
       andIclickRadioOptionXOnTheDoYouWantToAddAnyCommentsForTheAdditionalInformationPage("Yes")
@@ -433,7 +456,9 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I choose to click on the Remove link on the 'You have added 2 additional information for all items' page")
       andIchooseToClickOnTheXLinkOnTheYouHaveAddedXAdditionalInformationForAllItemsPage("Remove", "2")
 
-      And("I click radio option Yes on the 'Are you sure you want to remove this additional information from all items?' page")
+      And(
+        "I click radio option Yes on the 'Are you sure you want to remove this additional information from all items?' page"
+      )
       andIclickRadioOptionXOnTheAreYouSureYouWantToRemoveThisAdditionalInformationFromAllItemsPage("Yes")
 
       And("I click radio option No on the 'You have added 1 additional information for all items' page")
@@ -492,8 +517,12 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I choose radio option No on the Do you want to add an inland mode of transport? page")
       andIchooseRadioOptionXOnTheDoYouWantToAddAnInlandModeOfTransportPage("No")
 
-      And("I choose radio option Name of a sea-going vessel on the inland mode 'Which identification do you want to use for this vehicle?' page")
-      andIchooseRadioOptionXOnTheInlandModeWhichIdentificationDoYouWantToUseForThisVehiclePage("Name of a sea-going vessel")
+      And(
+        "I choose radio option Name of a sea-going vessel on the inland mode 'Which identification do you want to use for this vehicle?' page"
+      )
+      andIchooseRadioOptionXOnTheInlandModeWhichIdentificationDoYouWantToUseForThisVehiclePage(
+        "Name of a sea-going vessel"
+      )
 
       And("I enter registration number GB1234567 on the What is the identification number for this? page")
       andIenterRegistrationNumberOnTheWhatIsTheIdentificationNumberForThisPage("GB1234567")
@@ -516,7 +545,6 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
 
       And("I submit on the Check your answers section Modes and means of transport page")
       andIsubmitOnTheCheckYourAnswersSectionXPage("Modes and means of transport")
-
 
       //      ## Supply chain actor
       And("I choose radio option No on the Do you want to add a supply chain actor for all items? page")
@@ -588,7 +616,7 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
 
     }
 
-      Scenario("03 Northern Ireland office of departure with declaration type TIR and additional declaration type D") {
+    Scenario("03 Northern Ireland office of departure with declaration type TIR and additional declaration type D") {
 
       And("the user has submitted transportDetailsXI.json for LRN 1234567890TDXI and EORI number 1234567890")
       whenTheUserHasSubmittedFileForLRNAndEORINumber("transportDetailsXI.json", "1234567890TDXI", "1234567890")
@@ -622,20 +650,25 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
 
       //      ## Container indicator
       And(
-      "I choose radio option Not sure on the Are the goods being transported to another country after the end of this transit movement? page")
-      andIchooseRadioOptionXOnTheAreTheGoodsBeingTransportedToAnotherCountryAfterTheEndOfThisTransitMovementPage("Not sure")
+        "I choose radio option Not sure on the Are the goods being transported to another country after the end of this transit movement? page"
+      )
+      andIchooseRadioOptionXOnTheAreTheGoodsBeingTransportedToAnotherCountryAfterTheEndOfThisTransitMovementPage(
+        "Not sure"
+      )
 
       And("I choose radio option Not sure on the Are you using any shipping containers to transport the goods? page")
       andIchooseRadioOptionXOnTheAreYouUsingAnyShippingContainersToTransportTheGoodsPage("Not sure")
 
-          //       ## Inland mode of transport
+      //       ## Inland mode of transport
       And("I choose radio option Yes on the Do you want to add an inland mode of transport? page")
       andIchooseRadioOptionXOnTheDoYouWantToAddAnInlandModeOfTransportPage("Yes")
 
       And("I choose radio option Rail on the 'Which inland mode of transport are you using?' page")
       andIchooseRadioOptionXOnTheWhichInlandModeOfTransportAreYouUsingPage("Rail")
 
-      And("I choose radio option Train number on the inland mode 'Which identification do you want to use for this vehicle?' page")
+      And(
+        "I choose radio option Train number on the inland mode 'Which identification do you want to use for this vehicle?' page"
+      )
       andIchooseRadioOptionXOnTheInlandModeWhichIdentificationDoYouWantToUseForThisVehiclePage("Train number")
 
       And("I enter registration number 1234567 on the What is the identification number for this? page")
@@ -660,15 +693,18 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
       And("I choose radio option Yes on the border means Do you want to add identification for this vehicle? page")
       andIchooseRadioOptionXOnTheBorderMeansDoYouWantToAddIdentificationForThisVehiclePage("Yes")
 
-      And("I choose radio option registration number of the aircraft on the border mode 'Which identification do you want to use for this vehicle?' page")
-      andIchooseRadioOptionXOnTheBorderModeWhichIdentificationDoYouWantToUseForThisVehiclePage("registration number of the aircraft")
+      And(
+        "I choose radio option registration number of the aircraft on the border mode 'Which identification do you want to use for this vehicle?' page"
+      )
+      andIchooseRadioOptionXOnTheBorderModeWhichIdentificationDoYouWantToUseForThisVehiclePage(
+        "registration number of the aircraft"
+      )
 
       And("I enter identification GB1234567 on the What is the identification number for this? page")
       andIenterIdentificationOnTheWhatIsTheIdentificationNumberForThisPage("GB1234567")
 
       And("I select United Kingdom on the border mode What country is this vehicle registered to? page")
       andIselectOnTheBorderModeWhatCountryIsThisVehicleRegisteredToPage("United Kingdom")
-
 
       // ## Border means office of transit
       And("I select BARI on the 'Where is the customs office at the border?' page")
@@ -701,7 +737,7 @@ class TransportDetailsSpec extends BaseSpec with GivenWhenThen {
 //          And("I enter the date on the When do you expect the transit to arrive in AEROPORTO (IT262101)? page")
 //          andIenterTheDateOnTheWhenDoYouExpectTheTransitToArriveInXPage("AEROPORTO (IT262101)")
 
-        //      ## Carrier
+      //      ## Carrier
       And("I choose radio option No on the Do you want to add a carrier? page")
       andIChooseRadioOptionXOnTheDoYouWantToAddACarrierPage("No")
 
