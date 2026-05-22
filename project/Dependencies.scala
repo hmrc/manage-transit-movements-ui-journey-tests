891fb1556cc4dcccc455553a944a4919813411f4
JSquires91 <57271478+JSquires91@users.noreply.github.com>
2026-05-22 09:53:38 +0100

    Update test runner version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"         %% "ui-test-runner"         % "0.52.0",
+    "uk.gov.hmrc"         %% "ui-test-runner"         % "0.54.0",
     "org.scalatest"       %% "scalatest"              % "3.2.19",
     "com.vladsch.flexmark" % "flexmark-all"           % "0.64.8",
     "junit"                % "junit"                  % "4.13.2",
